738d740c98508037c27a6fb14c79c5e8fb9f6d50
Aman Singh <singhaman11415@gmail.com>
2026-03-04 08:53:16 +0530

    Frst Commit

---
+++ src/test/kotlin/com/newklio/opensec/OpensecApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.newklio.opensec
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpensecApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
